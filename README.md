17215b5fef50ac3723d5e682ebc6958dfe066176
imacoolgirlyo <tjrud1438@naver.com>
2024-05-31 09:27:18 +0900

    add ticket-calculator readme

--- README.md
+++ README.md
@@ -20,3 +20,19 @@ url: /flex
 부모가 flex container 일 때, flex item들의 기본 사이즈를 더해도 영역이 남는 경우가 있다. 이때 만약 남은 영역을 다 채우게끔 만들어버리고 싶다면? 👉 자식에게 `flex-grow` 먹여서 얼만큼 남은 영역들을 차지하게끔 할것인지 설정
 
 </details>
+
+### Object 에 익숙해지기
+
+0. Object 책 1,2,3장을 읽고 객체 지향의 기본 개념에 대해서 알아봅니다.
+
+1. 영화 예매 시스템의 도메인들을 생각해보고 이를 객체로 만들어봅니다. url: `/oop/ticketApplication`
+
+- js에서의 class 생성, 사용, 접근 제어자 등에도 익숙해집니다.
+
+2. 실제 컴포넌트에서 할인 정책에 따라 할인된 영화 금액들을 보여주고 싶습니다. 컴포넌트 내에 이리 저리 흩어져있는 중요 비즈니스 로직들을 한 곳에서 관리 할 수 있도록 리팩토링 해봅시다.
+   url: `/ticket-calculator`
+
+- calculateDiscountPrice 함수를 screening 객체 내에서 처리할 수 있도록 바꿔봅시다.
+- screening.getDiscountAmount 에서는 discountPolicy의 타입에 따라 총 할인 금액을 계산하는 방식을 다르게 처리하고 있습니다.
+  만약, 할인 정책이 더 늘어난다면 discountPolicy type도 늘어날 것이고 그에 따라 계산하는 방식들도 더 추가 됩니다. 할인 금액을 계산하는 방식을 꼭 screening이 알아야할까요? discountPolicy가 직접 계산하게끔 리팩토링 해봅시다.
+  - DiscountPolicy interface 생성, AmountDiscountPolicy, PercentDiscountPolicy 만들기
---
+++ src/app/ticket-calculator/page2.tsx
@@ -0,0 +1,224 @@
+"use client";
+
+import emotionStyled from "@emotion/styled";
+import { useState } from "react";
+
+interface DiscountPolicy {
+  getDiscountPrice(price: number): number;
+}
+
+class AllDiscountPolicy implements DiscountPolicy {
+  constructor() {}
+
+  getDiscountPrice(price: number): number {
+    return price;
+  }
+}
+
+class AmountDiscountPolicy implements DiscountPolicy {
+  discountAmount: number;
+
+  constructor(discountAmount: number) {
+    this.discountAmount = discountAmount;
+  }
+
+  getDiscountPrice(price: number): number {
+    return this.discountAmount;
+  }
+}
+
+class PercentDiscountPolicy implements DiscountPolicy {
+  discountPercent: number;
+
+  constructor(discountPercent: number) {
+    this.discountPercent = discountPercent;
+  }
+
+  getDiscountPrice(price: number): number {
+    return price * (this.discountPercent / 100);
+  }
+}
+
+const amountDiscountPolicy = new AmountDiscountPolicy(1000);
+const percentDiscountPolicy = new PercentDiscountPolicy(10);
+
+interface DiscountCondition {
+  type: "SEQUENCE" | "PERIOD";
+  isSatisfiedBy(screening: Screening): boolean;
+}
+
+interface Screening {
+  whenScreened: Date;
+  sequence: number;
+
+  title: string; // 영화 제목, 가격, discountPolicy은 Movie 객체로 분리할 수 있으나, 여기서는 간단하게 처리
+  price: number;
+  discountPolicy: DiscountPolicy;
+  getDiscountAmount(condition: DiscountCondition[]): number;
+}
+
+function TicketCalculator() {
+  const [screeningToBuy, setScreeningToBuy] = useState<Screening | null>(null);
+
+  const screenings: Screening[] = [
+    {
+      title: "Dune 2",
+      sequence: 1,
+      price: 15000,
+      whenScreened: new Date(2024, 4, 2),
+      discountPolicy: amountDiscountPolicy,
+      getDiscountAmount(conditions) {
+        const conditionSatisfied = conditions.find((condition) =>
+          condition.isSatisfiedBy(this as Screening)
+        );
+
+        if (!conditionSatisfied) {
+          return 0;
+        }
+
+        return this.discountPolicy.getDiscountPrice(this.price);
+      },
+    },
+    {
+      title: "혹성탈출",
+      sequence: 2,
+      price: 20000,
+      whenScreened: new Date(2024, 5, 6),
+      discountPolicy: percentDiscountPolicy,
+      getDiscountAmount(conditions) {
+        const conditionSatisfied = conditions.find((condition) =>
+          condition.isSatisfiedBy(this as Screening)
+        );
+
+        if (!conditionSatisfied) {
+          return 0;
+        }
+
+        // this.discountPolicy.getDiscountPrice
+
+        return this.discountPolicy.getDiscountPrice(this.price);
+      },
+    },
+    {
+      title: "설계자",
+      sequence: 2,
+      price: 20000,
+      whenScreened: new Date(2024, 5, 3),
+      discountPolicy: new AllDiscountPolicy(),
+      getDiscountAmount(conditions) {
+        const conditionSatisfied = conditions.find((condition) =>
+          condition.isSatisfiedBy(this as Screening)
+        );
+
+        if (!conditionSatisfied) {
+          return 0;
+        }
+
+        return this.discountPolicy.getDiscountPrice(this.price);
+      },
+    },
+  ];
+
+  const conditions: DiscountCondition[] = [
+    {
+      type: "SEQUENCE",
+      isSatisfiedBy: (screening: Screening) => {
+        return screening.sequence === 1;
+      },
+    },
+    {
+      type: "PERIOD",
+      isSatisfiedBy: (screening: Screening) => {
+        return screening.whenScreened > new Date(2024, 5, 5);
+      },
+    },
+  ];
+
+  // condition.getMovieFee()
+  // screening.getMovieFee()
+
+  // const calculateDiscountPrice = (
+  //   screening: Screening | null,
+  //   conditions: DiscountCondition[]
+  // ): number => {
+  //   if (!screening) {
+  //     return 0;
+  //   }
+
+  //   const conditionSatisfied = conditions.find((condition) =>
+  //     condition.isSatisfiedBy(screening)
+  //   );
+
+  //   if (!conditionSatisfied) {
+  //     return 0;
+  //   }
+
+  //   if (screening.discountPolicy.type === "AMOUNT_DISCOUNT") {
+  //     return screening.discountPolicy.discountAmount!;
+  //   } else {
+  //     return (
+  //       screening.price * (screening.discountPolicy.discountPercent! / 100)
+  //     );
+  //   }
+  // };
+
+  // const calculateDiscountedPrice = (
+  //   screening: Screening | null,
+  //   conditions: DiscountCondition[]
+  // ): number => {
+  //   if (!screening) {
+  //     return 0;
+  //   }
+
+  //   const discountPrice = calculateDiscountPrice(screening, conditions);
+  //   return screening.price - discountPrice;
+  // };
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        {screenings.map((screening) => (
+          <ScreeningItem
+            key={screening.title}
+            style={{
+              border: "1px solid black",
+              padding: "10px",
+              margin: "10px",
+            }}
+            onClick={() => setScreeningToBuy(screening)}
+          >
+            <div>{screening.title}</div>
+            <div>{screening.price}원</div>
+            {/* <div>
+              {screening.discountPolicy.type === "AMOUNT_DISCOUNT"
+                ? `${screening.discountPolicy.discountAmount}원 할인`
+                : `${screening.discountPolicy.discountPercent}% 할인`}
+            </div> */}
+          </ScreeningItem>
+        ))}
+      </div>
+      <div>
+        <div>총 결제 금액: {screeningToBuy ? screeningToBuy.price : ""}원</div>
+        <div>할인 금액: {screeningToBuy?.getDiscountAmount(conditions)}원</div>
+        <div>
+          {/* 할인 후 금액: {screeningToBuy.getTotal(screeningToBuy?.price, conditions)}원 */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TicketCalculator;
+
+const ScreeningItem = emotionStyled.div`
+    border: 1px solid black;
+    padding: "10px";
+    margin: "10px";
+    :hover {
+        background-color: lightgray;
+    }
+`;
